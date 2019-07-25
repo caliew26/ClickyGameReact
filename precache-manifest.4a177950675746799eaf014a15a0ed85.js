bb48896af63b411ca70bb79fdf84c38d30cfad40
Adventure 2019 <caliew26@gmail.com>
2019-07-25 11:23:05 -0400

    Updates

---
+++ precache-manifest.4a177950675746799eaf014a15a0ed85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ca259a752fdc6fb961e9cdaebc6e0f",
+    "url": "/caliew26/ClickyGameReact/index.html"
+  },
+  {
+    "revision": "b34fa2ab90fdcf433c41",
+    "url": "/caliew26/ClickyGameReact/static/css/main.05aa9ef5.chunk.css"
+  },
+  {
+    "revision": "cf30aea864ed6bdd3290",
+    "url": "/caliew26/ClickyGameReact/static/js/2.9384f8d8.chunk.js"
+  },
+  {
+    "revision": "b34fa2ab90fdcf433c41",
+    "url": "/caliew26/ClickyGameReact/static/js/main.92352906.chunk.js"
+  },
+  {
+    "revision": "496b264193fcf8ae014f",
+    "url": "/caliew26/ClickyGameReact/static/js/runtime~main.f9509027.js"
+  }
+]);
